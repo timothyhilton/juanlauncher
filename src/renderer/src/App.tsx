557279db86f0a3a1465f5c4a4d3f8504fd67417d
timothyhilton <recore444@gmail.com>
2024-07-11 21:27:51 +1000

    styling

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -10,11 +10,14 @@ export default function App(){
   }
 
   return(
-    <div className="">
-      <VersionDropdown value={build} setValue={setBuild} />
-      <Button onClick={onClick}>
-        launch
-      </Button>
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+      <div style={{ textAlign: 'center' }}>
+        <h1 className="font-bold text-5xl mb-5">juan launcher</h1>
+        <VersionDropdown value={build} setValue={setBuild} />
+        <Button onClick={onClick} variant="outline">
+          launch
+        </Button>
+      </div>
     </div>
   )
 }
